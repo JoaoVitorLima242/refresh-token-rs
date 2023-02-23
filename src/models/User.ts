47ea7b17d1fe17426111b9facda7cf9dd2354ba0
JoaoVitorLima242 <jlima0500@gmail.com>
2023-02-23 11:34:05 -0300

    unique username

--- src/models/User.ts
+++ src/models/User.ts
@@ -7,7 +7,7 @@ export interface IUser {
 
 const userSchema = new Schema<IUser>(
   {
-    username: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
   {
